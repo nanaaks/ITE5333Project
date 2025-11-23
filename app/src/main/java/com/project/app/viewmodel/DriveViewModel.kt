2ddf6bdc68d0dc0440226c36400ab7511cb0bba8
PLLV99 <ppteacher2563@gmail.com>
2025-11-23 02:17:15 -0500

    add and update DriveViewModel

---
+++ app/src/main/java/com/project/app/viewmodel/DriveViewModel.kt
@@ -0,0 +1,27 @@
+package com.project.app.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.project.app.data.Booking
+import com.project.app.data.RideRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+class DriveViewModel : ViewModel() {
+    private val _jobs = MutableStateFlow<List<Booking>>(emptyList())
+    val jobs: StateFlow<List<Booking>> = _jobs
+
+    init {
+        refreshJobs()
+    }
+    fun acceptJob(id: String) {
+        RideRepository.updateJobStatus(id, "Accepted")
+        refreshJobs()
+    }
+    fun declineJob(id: String) {
+        RideRepository.updateJobStatus(id, "Declined")
+        refreshJobs()
+    }
+    fun refreshJobs() {
+        _jobs.value = RideRepository.getAllBookings()
+    }
+}
