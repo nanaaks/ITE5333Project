6fdb679a02db3fcc26833b4278b7a6a1df46e2a8
PLLV99 <ppteacher2563@gmail.com>
2025-11-23 04:09:44 -0500

    add bottombar logic from HomeScreen

--- app/src/main/java/com/project/app/nav/TabNavGraph.kt
+++ app/src/main/java/com/project/app/nav/TabNavGraph.kt
@@ -1,34 +1,99 @@
 package com.project.app.nav
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Badge
+import androidx.compose.material3.BadgedBox
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import com.project.app.view.HomeTabScreen
+import androidx.navigation.compose.currentBackStackEntryAsState
 import com.project.app.view.RideScreen
 import com.project.app.view.DriveScreen
+import com.project.app.view.HomeScreen
+import com.project.app.viewmodel.DriveViewModel
 import com.project.app.viewmodel.UserViewModel
 
 @Composable
 fun TabNavGraph(
     navHostController: NavHostController,
     tabNavController: NavHostController,
-    userVM : UserViewModel
+    userVM : UserViewModel,
+    driveVM : DriveViewModel
 ) {
-    NavHost(
-        tabNavController,
-        startDestination = TabRoutes.Home.routeName
-    ) {
-        composable(TabRoutes.Home.routeName) {
-            HomeTabScreen(navHostController, tabNavController, userVM)
-        }
+    val screens = listOf(TabRoutes.Home, TabRoutes.Ride, TabRoutes.Drive)
+    val navBackStackEntry by tabNavController.currentBackStackEntryAsState()
+    val currentDestination = navBackStackEntry?.destination
 
-        composable(TabRoutes.Ride.routeName) {
-            RideScreen(navHostController)
-        }
+    Scaffold(
+        bottomBar = {
+            NavigationBar {
+                screens.forEach { screen ->
+                    NavigationBarItem(
+                        label = { Text(screen.title) },
+                        icon = {
+                            Icon(
+                                screen.icon,
+                                contentDescription = screen.title
+                            )
+                            BadgedBox(
+                                badge = {
+                                    if (screen.badgeCount > 0) {
+                                        Badge() {
+                                            Text(screen.badgeCount.toString())
+                                        }
+                                    }
+                                }
+                            ) {
+                                Icon(
+                                    screen.icon,
+                                    contentDescription = screen.title
+                                )
+                            }
+                        },
+                        selected = currentDestination?.hierarchy?.any { it.route == screen.routeName } == true,
+                        onClick = {
+                            tabNavController.navigate(screen.routeName) {
+                                //remove previously visited tab from the Navigation Stack
+                                popUpTo(tabNavController.graph.findStartDestination().id) {
+                                    //and save any data
+                                    saveState = true
+                                }
+                                launchSingleTop = true
+                                restoreState = true //reload previously saved data
+                            }
+                        }
+                    )
+                }//NavigationBarItem
+            }//NavigationBar
+        }//bottomBar
+    )//Scaffold
+    { innerPadding ->
+        NavHost(
+            tabNavController,
+            startDestination = TabRoutes.Home.routeName,
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            composable(TabRoutes.Home.routeName) {
+                HomeScreen(navHostController, tabNavController, userVM, driveVM)
+            }
 
-        composable(TabRoutes.Drive.routeName) {
-            DriveScreen(navHostController)
+            composable(TabRoutes.Ride.routeName) {
+                RideScreen(navHostController, driveVM)
+            }
+
+            composable(TabRoutes.Drive.routeName) {
+                DriveScreen(navHostController, driveVM)
+            }
         }
     }
 }
