a8d0a668170ead3a63b01a0adeaa3ce429b205c2
PLLV99 <ppteacher2563@gmail.com>
2025-12-02 22:50:10 -0500

    update Settings Screen

--- app/src/main/java/com/project/app/view/SettingsScreen.kt
+++ app/src/main/java/com/project/app/view/SettingsScreen.kt
@@ -18,12 +18,15 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Info
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -50,105 +53,115 @@ fun SettingsScreen(
 
     var showBottomSheet by remember { mutableStateOf(false) }
     val sheetState = rememberModalBottomSheetState()
-
-    Column (
-        verticalArrangement = Arrangement.spacedBy(16.dp),
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp)
-    ) {
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Text("Settings",
-            style = MaterialTheme.typography.titleLarge.copy(
-                fontSize = 32.sp
-            )
-        )
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Text("Dark Mode: ${if (state.darkMode) "ON" else "OFF"}" ,
-                style = MaterialTheme.typography.labelLarge.copy(
-                    fontSize = 24.sp,
-                ))
-            Switch(
-                checked = state.darkMode,
-                onCheckedChange =  {
-                    settingsVM.enableDarkMode(!state.darkMode)
-                }
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = { Text("Settings") },
+                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.onPrimaryContainer
+                )
             )
         }
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
+    ) { innerPadding ->
+        Column(
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .padding(16.dp)
         ) {
-            Text("Notifications: ${if (state.notify) "ON" else "OFF"}" ,
-                style = MaterialTheme.typography.labelLarge.copy(
-                    fontSize = 24.sp,
-                ))
-            Switch(
-                checked = state.notify,
-                onCheckedChange = {
-                    settingsVM.enableNotifications(!state.notify)
-                }
-            )
-        }
-        Button(
-            onClick = {
-                settingsVM.saveSettings(state.darkMode, state.notify)
-            },
-            modifier = Modifier.fillMaxWidth(),
-            colors = ButtonDefaults.buttonColors(
-                containerColor = MaterialTheme.colorScheme.primary,
-                contentColor = MaterialTheme.colorScheme.onPrimary
-            )
-        ) {
-            Text("Save", style = MaterialTheme.typography.labelLarge.copy(fontSize = 24.sp,))
-        }
-        Button(
-            onClick = {
-                settingsVM.resetSettings()
-            },
-            modifier = Modifier.fillMaxWidth(),
-            colors = ButtonDefaults.buttonColors(
-                containerColor = MaterialTheme.colorScheme.error,
-                contentColor = MaterialTheme.colorScheme.onError
-            )
-        ) {
-            Text("Reset", style = MaterialTheme.typography.labelLarge.copy(fontSize = 24.sp,))
-        }
-        Button(
-            modifier = Modifier.fillMaxWidth(),
-            onClick = {
-                //if the bottom sheet is not already visible then show it
-                if (!sheetState.isVisible) {
-                    showBottomSheet = true //show the bottom sheet
-                } else {
-                    showBottomSheet = false
-                }
-            }) {
-            Text("About App")
-        }//Button
-
-        Spacer(modifier = Modifier.height(16.dp))
-        if (showBottomSheet){
-            ModalBottomSheet(
-                onDismissRequest = { showBottomSheet = false },
-                sheetState = sheetState
+            Spacer(modifier = Modifier.height(16.dp))
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
             ) {
-                BottomSheetContent(
-                    icon = Icons.Default.Info,
-                    title = "App Information",
-                    message = "App Name: PickApp\n" +
-                            "Developer Name: Group1\n" +
-                            "App Version: 0.04"
-                )//BottomSheetContent
-            }//ModalBottomSheet
-        }//if
-    }
+                Text(
+                    "Dark Mode: ${if (state.darkMode) "ON" else "OFF"}",
+                    style = MaterialTheme.typography.labelLarge.copy(
+                        fontSize = 24.sp,
+                    )
+                )
+                Switch(
+                    checked = state.darkMode,
+                    onCheckedChange = {
+                        settingsVM.enableDarkMode(!state.darkMode)
+                    }
+                )
+            }
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    "Notifications: ${if (state.notify) "ON" else "OFF"}",
+                    style = MaterialTheme.typography.labelLarge.copy(
+                        fontSize = 24.sp,
+                    )
+                )
+                Switch(
+                    checked = state.notify,
+                    onCheckedChange = {
+                        settingsVM.enableNotifications(!state.notify)
+                    }
+                )
+            }
+            Button(
+                onClick = {
+                    settingsVM.saveSettings(state.darkMode, state.notify)
+                },
+                modifier = Modifier.fillMaxWidth(),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = MaterialTheme.colorScheme.primary,
+                    contentColor = MaterialTheme.colorScheme.onPrimary
+                )
+            ) {
+                Text("Save", style = MaterialTheme.typography.bodyLarge)
+            }
+            Button(
+                onClick = {
+                    settingsVM.resetSettings()
+                },
+                modifier = Modifier.fillMaxWidth(),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = MaterialTheme.colorScheme.error,
+                    contentColor = MaterialTheme.colorScheme.onError
+                )
+            ) {
+                Text("Reset", style = MaterialTheme.typography.bodyLarge)
+            }
+            Button(
+                modifier = Modifier.fillMaxWidth(),
+                onClick = {
+                    //if the bottom sheet is not already visible then show it
+                    if (!sheetState.isVisible) {
+                        showBottomSheet = true //show the bottom sheet
+                    } else {
+                        showBottomSheet = false
+                    }
+                }) {
+                Text("About App",style = MaterialTheme.typography.bodyLarge)
+            }//Button
+
+            Spacer(modifier = Modifier.height(16.dp))
+            if (showBottomSheet) {
+                ModalBottomSheet(
+                    onDismissRequest = { showBottomSheet = false },
+                    sheetState = sheetState
+                ) {
+                    BottomSheetContent(
+                        icon = Icons.Default.Info,
+                        title = "App Information",
+                        message = "App Name: PickApp\n" +
+                                "Developer Name: Group1\n" +
+                                "App Version: 0.04"
+                    )//BottomSheetContent
+                }//ModalBottomSheet
+            }//if
+        }// Column
+    }//innerPadding
 }
 
 @Composable
@@ -169,20 +182,19 @@ fun BottomSheetContent(
         Row (
             modifier = Modifier.fillMaxWidth()
         ){
-
             Icon(
                 imageVector = icon,
                 contentDescription = null,
                 tint = MaterialTheme.colorScheme.primary,
                 modifier = Modifier.size(30.dp)
             )
-
             Spacer(modifier = Modifier.width(12.dp))
             Column (modifier = Modifier.fillMaxWidth()){
-                Text(title, style = MaterialTheme.typography.titleLarge,color = MaterialTheme.colorScheme.onSurface)
+                Text(title, style = MaterialTheme.typography.titleLarge, color = MaterialTheme.colorScheme.onSurface)
 
                 Spacer(modifier = Modifier.width(8.dp))
-                Text(message, style = MaterialTheme.typography.bodyLarge,    color = MaterialTheme.colorScheme.onSurface)
+
+                Text(message, style = MaterialTheme.typography.bodyLarge, color = MaterialTheme.colorScheme.onSurface)
             }//Column
         }//Row
     }//Column
